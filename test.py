7fd259656c380aaf275ed57510502260bd766b0b
mebratu <mebratucheka7@gmail.com>
2026-08-05 13:18:38 +0300

    removed unimportant file and added important one

--- test.py
+++
@@ -1,9 +0,0 @@
-from tools.search_tool import duckduckgo_search
-from tools.flight_tool import search_flights
-
-# results = duckduckgo_search("What are best hotels in Addis Ababa?")
-# for result in results:
-#     print(result)
-# "Plan a 7 days Japan trip from Bangladesh"
-result = search_flights("Plan a 7 days Japan trip from Ethiopia")
-print(result)
